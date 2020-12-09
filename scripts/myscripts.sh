3a5e92a8677f5240875b122a684e5b03ce4cdfa3
MatrixDeity <dt9595@mail.ru>
2020-12-09 15:07:09 +0300

    Fix gitdrop
    Add --force to 'git submodules'

--- scripts/myscripts.sh
+++ scripts/myscripts.sh
@@ -13,6 +13,6 @@ alias upbash="vim ${HOME}/.bashrc && source ${HOME}/.bashrc"
 alias vsync="${MYSCRIPTS_DIR}/vsync.sh"
 alias vvisit="${MYSCRIPTS_DIR}/vvisit.sh"
 
-function gitdrop { git clean -fd && git reset --hard HEAD~${1:-0} && git submodule update --init --recursive; }
+function gitdrop { git clean -fd && git reset --hard HEAD~${1:-0} && git submodule update --init --recursive --force; }
 function lg { ls -la "${2:-.}" | grep --color=no "${1}"; }
 function mkcd { mkdir -p ${1} && cd ${1}; }
